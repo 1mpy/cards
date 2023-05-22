31195830a435c57c8015b4d4a879e5442b102acf
Anton <Impl3x@yandex.ru>
2023-05-22 21:10:18 +0300

    window.app+render+components+submit

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
-const difficultyLevels = document.querySelectorAll(".difficulty__type");
-const startButton = document.querySelector(".game__menu-button");
-// console.log(difficultyLevels);
-// console.log(startButton);
-startButton.addEventListener("click", () => {
-  for (const difficultyLevel of difficultyLevels) {
-    if (difficultyLevel.checked) {
-      localStorage.setItem('game', difficultyLevel.value);
-      document.body.innerHTML = `<h1>Выбран уровень сложности ${difficultyLevel.value}</h2>`;
-    } 
-  }
-});
+import {gameDifficulty} from "./difficulty-selection.js"
+
+const appEl = document.getElementById('app');
+
+gameDifficulty(appEl);
